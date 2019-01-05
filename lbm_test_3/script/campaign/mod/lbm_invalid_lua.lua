566f23b9f2905ac12a17707f56473c7b4eb47b4c
lbmaian <maian330@gmail.com>
2019-01-05 02:04:40 -0800

    Add invalid lua script to test mod_scripting handling of it

---
+++ lbm_test_3/script/campaign/mod/lbm_invalid_lua.lua
@@ -0,0 +1,5 @@
+out("lbm_invalid_lua: this shouldn't run since this script has syntax errors, but the load error should still be logged")
+
+function hi there this is obviously invalid lua()
+    Vandy wtf
+end
